9461e986be0df21f4310ac21bc256a717354fc7c
SergeyGolenko <1989sergiotay@gmail.com>
2021-02-05 17:50:31 +0200

    добавил код к кнопке (код меняет рандомно картинку кубика)

--- DiceeSimpleGame/ViewController.swift
+++ DiceeSimpleGame/ViewController.swift
@@ -13,15 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let random = Int.random(in: 1...6)
+        let random2 = Int.random(in: 1...6)
+        diceImageView1.image = UIImage(named: "dice\(random)")
+        diceImageView2.image = UIImage(named: "dice\(random2)")
     }
     
 }
